a792aad5ae80f5240f70a6eee44637573e939994
michaelcordero <michaelpetercordero@gmail.com>
2018-10-07 19:17:06 -0500

    Reorganizing & Updating

---
+++ CoreDataStructures/Classes/List.swift
@@ -0,0 +1,21 @@
+//
+//  List.swift
+//  CoreDataStructures
+//
+//  Created by Michael Cordero on 10/7/18.
+//
+
+import Foundation
+
+protocol List {
+    associatedtype T
+    func size() -> Int
+    func add(_ element: T) -> Bool // return true if element can be added
+    func remove(_ element: T) -> T? // return the removed element
+    func get(_ index: Int ) -> T?    // return the element at the specified index
+    func set(_ index: Int, value: T) -> T?   // sets a new value at specified index, returns old value
+    func contains(_ element: T) -> Bool
+    func indexOf(_ element: T) -> Int?
+    func clear() -> Void
+    func sort( comparator: (T,T) throws -> Bool ) -> Void
+}
